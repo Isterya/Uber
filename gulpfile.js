a37d28d7b6fd46ec8a4af994ce288c41bfc60329
isterya <vash.vybor.1@gmail.com>
2025-05-10 19:27:37 +0200

    fix: fix problem with displaying images on the site

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,17 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const del = require('del');
 
+// Очистка папки dist
 gulp.task('clean', function () {
   return del(['dist']);
 });
 
+// HTML
 gulp.task('html', function () {
   return gulp.src('src/*.html').pipe(gulp.dest('dist'));
 });
 
+// Стили
 gulp.task('styles', function () {
   return gulp
     .src('src/sass/**/*.+(scss|sass)')
@@ -23,15 +26,44 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('assets', function () {
-  return gulp.src('src/assets/**/*').pipe(gulp.dest('dist/assets'));
+// Сторонние стили (если используешь например bootstrap)
+gulp.task('css-libs', function () {
+  return gulp.src('src/css/**/*.css').pipe(gulp.dest('dist/css'));
 });
 
+// JS
 gulp.task('scripts', function () {
   return gulp.src('src/js/**/*').pipe(gulp.dest('dist/js'));
 });
 
+// Шрифты
+gulp.task('fonts', function () {
+  return gulp.src('src/assets/fonts/**/*').pipe(gulp.dest('dist/assets/fonts'));
+});
+
+// Иконки
+gulp.task('icons', function () {
+  return gulp.src('src/assets/icons/**/*').pipe(gulp.dest('dist/assets/icons'));
+});
+
+// Изображения
+gulp.task('images', function () {
+  return gulp.src('src/assets/img/**/*').pipe(gulp.dest('dist/assets/img'));
+});
+
+// Финальная сборка
 gulp.task(
   'build',
-  gulp.series('clean', gulp.parallel('html', 'styles', 'scripts', 'assets'))
+  gulp.series(
+    'clean',
+    gulp.parallel(
+      'html',
+      'styles',
+      'css-libs',
+      'scripts',
+      'fonts',
+      'icons',
+      'images'
+    )
+  )
 );
