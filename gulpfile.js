ed36e15108f71cae96c40070237f9a366352fd4b
isterya <vash.vybor.1@gmail.com>
2025-05-10 18:58:24 +0200

    chore: prepare the project for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,37 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
+const del = require('del');
 
-gulp.task('server', function () {
-   browserSync({
-      server: {
-         baseDir: 'src',
-      },
-   });
+gulp.task('clean', function () {
+  return del(['dist']);
+});
 
-   gulp.watch('src/*.html').on('change', browserSync.reload);
+gulp.task('html', function () {
+  return gulp.src('src/*.html').pipe(gulp.dest('dist'));
 });
 
 gulp.task('styles', function () {
-   return gulp
-      .src('src/sass/**/*.+(scss|sass)')
-      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-      .pipe(rename({ suffix: '.min', prefix: '' }))
-      .pipe(autoprefixer())
-      .pipe(cleanCSS({ compatibility: 'ie8' }))
-      .pipe(gulp.dest('src/css'))
-      .pipe(browserSync.stream());
+  return gulp
+    .src('src/sass/**/*.+(scss|sass)')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch', function () {
-   gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
+gulp.task('assets', function () {
+  return gulp.src('src/assets/**/*').pipe(gulp.dest('dist/assets'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('scripts', function () {
+  return gulp.src('src/js/**/*').pipe(gulp.dest('dist/js'));
+});
+
+gulp.task(
+  'build',
+  gulp.series('clean', gulp.parallel('html', 'styles', 'scripts', 'assets'))
+);
